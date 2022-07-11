8290f0c389fd2c5df687ba82e026ce16471313b4
Shibli <ahmedrazashibli@gmail.com>
2022-07-11 16:29:34 +0530

    Adding JS work

---
+++ JavaScript/Day2/theDom.js
@@ -0,0 +1,28 @@
+// DOM manipulation
+
+// GetElementByID()
+
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// GetElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+
+// GetElementByTagName()
+
+const listItems = document.getElementsByTagName('li');
+console.log(listItems);
+
+// querySelector()
+
+const container = document.querySelector('div');
+console.log(container);
+
+
+// querySelectorAll() selects all
+
+const containers = document.querySelector('div');
+console.log(containers);
